5d89afe704fd0df8703a89c132c8d7fa5d41362e
eonepp <vaniapopov2004@mail.ru>
2023-09-23 18:57:17 +0300

    here we go again #123

--- ShelkovyPopov1/main.cpp
+++ ShelkovyPopov1/main.cpp
@@ -1,8 +1,19 @@
-﻿
+﻿#include <vector>
 #include <iostream>
-
+using namespace std;
+void cpp() {
+    int a;
+    a = 1;
+    float b = 1.0f;
+    char y = '♂';
+    double b1 = 1.0;
+    cout << y;
+    return;
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
+    cpp();
+    return 0;
 }
-
+ 
