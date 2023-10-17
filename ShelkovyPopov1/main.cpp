ee81239e344d7624f8d5643199378355fa94ae52
eonepp <vaniapopov2004@mail.ru>
2023-10-17 13:45:19 +0300

    #changes

--- ShelkovyPopov1/main.cpp
+++ ShelkovyPopov1/main.cpp
@@ -1,22 +1,27 @@
-﻿#include <vector>
-#include <iostream>
+﻿#include <iostream>
+#include "Header.h"
 using namespace std;
-void cpp() {
-    int a;
-    a = 1;
-    float b = 1.0f;
-    char y = '♂';
-    double b1 = 1.0;
-    y = b + b1;
-    cout << y;
-    return;
-}
-	ind = num_kFkS(pArray, n);
-	
-		
 
 
-
-    return 0;
+int main() {
+	int n, m;
+	cin >> n;
+	cin >> m;
+	int** pArray = new int* [n];
+	for (int i = 0; i < n; i++) {
+		pArray[i] = new int[m];
+	}
+	for (int i = 0; i<n;i++) 
+	{
+		for (int j =0;j< m; j++) 
+		{
+			cin >> pArray[i][j];
+		}
+	}
+	int *sums = multWithNegative(pArray, n, m);
+	int* ind = indexesOfNegative(pArray, n, m);
+	outElements(sums, countOfNegative(pArray, n, m));
+	outElements(ind, countOfNegative(pArray, n, m));
+	return 0;
 }
  
